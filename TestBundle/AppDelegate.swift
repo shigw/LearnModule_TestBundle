1c8086fc92e1d071b270a14dce6e95e8628f04a8
shigw <shigw@zbzs-imac.local>
2015-11-08 10:36:01 +0800

    Initial Commit

---
+++ TestBundle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestBundle
+//
+//  Created by shigw on 08/11/15.
+//  Copyright © 2015年 shigw. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
